2fc80c6e3c3ff4ac1ad73d4bbf4d6a62f09750d9
Dig-Doug <douglas.roeper@gmail.com>
2016-04-05 16:03:32 -0400

    Initial Commit
    Skeleton projects

---
+++ ec450_hw5_slave/main.c
@@ -0,0 +1,62 @@
+#include <msp430.h>
+
+void init_spi(void);
+
+void sendByte(unsigned char aData)
+{
+	UCB0TXBUF = aData;
+}
+
+//----------------------------------------------------------------
+
+// ======== Receive interrupt Handler for UCB0 ==========
+
+void interrupt spi_rx_handler(){
+	unsigned char data_received = UCB0RXBUF; // copy data to global variable
+	IFG2 &= ~UCB0RXIFG;		 // clear UCB0 RX flag
+}
+ISR_VECTOR(spi_rx_handler, ".int07")
+
+
+//Bit positions in P1 for SPI
+#define SPI_CLK 0x20
+#define SPI_SOMI 0x40
+#define SPI_SIMO 0x80
+
+// calculate the lo and hi bytes of the bit rate divisor
+#define BIT_RATE_DIVISOR 32
+#define BRLO (BIT_RATE_DIVISOR &  0xFF)
+#define BRHI (BIT_RATE_DIVISOR / 0x100)
+
+void init_spi(){
+	UCB0CTL1 = UCSSEL_2+UCSWRST;  		// Reset state machine; SMCLK source;
+	UCB0CTL0 = UCCKPH					// Data capture on rising edge
+			   							// read data while clock high
+										// lsb first, 8 bit mode,
+			   +UCMST					// master
+			   +UCMODE_0				// 3-pin SPI mode
+			   +UCSYNC;					// sync mode (needed for SPI or I2C)
+	UCB0BR0=BRLO;						// set divisor for bit rate
+	UCB0BR1=BRHI;
+	UCB0CTL1 &= ~UCSWRST;				// enable UCB0 (must do this before setting
+										//              interrupt enable and flags)
+	IFG2 &= ~UCB0RXIFG;					// clear UCB0 RX flag
+	IE2 |= UCB0RXIE;					// enable UCB0 RX interrupt
+	// Connect I/O pins to UCB0 SPI
+	P1SEL |=SPI_CLK+SPI_SOMI+SPI_SIMO;
+	P1SEL2|=SPI_CLK+SPI_SOMI+SPI_SIMO;
+}
+
+void main(){
+
+	WDTCTL = WDTPW + WDTHOLD;       // Stop watchdog timer
+	BCSCTL1 = CALBC1_8MHZ;			// 8Mhz calibration for clock
+  	DCOCTL  = CALDCO_8MHZ;
+
+  	init_spi();
+
+
+
+
+ 	_bis_SR_register(GIE+LPM0_bits);
+}

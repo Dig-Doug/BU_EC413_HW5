f4648d6614ee1951c19c5beb18b2d9dc15fed9da
Dig-Doug <douglas.roeper@gmail.com>
2016-04-11 16:32:51 -0400

    Merged uart code

--- ec450_hw5_master/main.c
+++ ec450_hw5_master/main.c
@@ -19,14 +19,9 @@
 #define HIGH 	1
 #define BUTTON	BIT3
 
-// Globals for printing
-unsigned int conPrint=500; // number of conversions for each print line
-unsigned int conPrintCounter; // down counter for printing conversions
-unsigned int conPrintSequence;// output line numbers
-// a buffer to construct short text output strings
-#define CBUFLEN 20
-char cbuffer[CBUFLEN] = "1234567891234567891"; // buffer for output of characters
 
+short hasUARTMessage = 0;
+char stringBuffer[20];
 
 //Globals for button
 short lastButtonState = HIGH;
@@ -36,6 +31,37 @@ volatile unsigned int answer = 30;
 volatile char state = 0;
 volatile unsigned char lower;
 volatile unsigned char foo;
+
+void writeString(char *string, const char * aPrint)
+{
+	do
+	{
+		*string = *aPrint;
+		string++;
+		aPrint++;
+	}
+	while (aPrint != '\0');
+}
+
+void writeInt(char * string, int aNum)
+{
+	string[5] = '\0';
+	int digit = aNum % 10;
+	string[4] = '0' + digit;
+	aNum /= 10;
+	digit = aNum % 10;
+	string[3] = '0' + digit;
+	aNum /= 10;
+	digit = aNum % 10;
+	string[2] = '0' + digit;
+	aNum /= 10;
+	digit = aNum % 10;
+	string[1] = '0' + digit;
+	aNum /= 10;
+	digit = aNum % 10;
+	string[0] = '0' + digit;
+	aNum /= 10;
+}
 
 
 void sendByte(unsigned char aData)
@@ -62,15 +88,15 @@ void init_WDT(){
 void init_spi(){
 	UCB0CTL1 = UCSSEL_2+UCSWRST;  		// Reset state machine; SMCLK source;
 	UCB0CTL0 = UCCKPH					// Data capture on rising edge
-			   							// read data while clock high
-										// lsb first, 8 bit mode,
-			   +UCMST					// master
-			   +UCMODE_0				// 3-pin SPI mode
-			   +UCSYNC;					// sync mode (needed for SPI or I2C)
+			// read data while clock high
+			// lsb first, 8 bit mode,
+			+UCMST					// master
+			+UCMODE_0				// 3-pin SPI mode
+			+UCSYNC;					// sync mode (needed for SPI or I2C)
 	UCB0BR0=BRLO;						// set divisor for bit rate
 	UCB0BR1=BRHI;
 	UCB0CTL1 &= ~UCSWRST;				// enable UCB0 (must do this before setting
-										//              interrupt enable and flags)
+	//              interrupt enable and flags)
 	IFG2 &= ~UCB0RXIFG;					// clear UCB0 RX flag
 	IE2 |= UCB0RXIE;					// enable UCB0 RX interrupt
 	// Connect I/O pins to UCB0 SPI
@@ -82,9 +108,9 @@ int genRandom() {
 
 	TA0CTL |= TACLR;            // reset clock
 	TA0CTL = TASSEL_2+ID_0+MC_1;// clock source = SMCLK
-	                            // clock divider=1
-	                            // UP mode
-	                            // timer A interrupt off
+	// clock divider=1
+	// UP mode
+	// timer A interrupt off
 	// compare mode, output mode 0, no interrupt enabled
 	TA0CCTL0=0;
 	TA0CCR0 = -1;
@@ -139,34 +165,25 @@ void interrupt spi_rx_handler(){
 		break;
 	case 9:{	//sending High(H)/Low(L)/Equal(E)
 		unsigned int guess = (foo << 8) + lower;
-		//u_print_string("Guess ");
-		int digit = (guess % 10000);
-		cbuffer[0] = '0' + (digit < 10 ? digit : 0);
-		guess /= 10;
-		digit = (guess % 1000);
-		cbuffer[1] = '0' + (digit < 10 ? digit : 0);
-		guess /= 10;
-		digit = (guess % 100);
-		cbuffer[2] = '0' + (digit < 10 ? digit : 0);
-		guess /= 10;
-		digit = (guess % 10);
-		cbuffer[3] = '0' + (digit < 10 ? digit : 0);
-		guess /= 10;
-		cbuffer[4] = '0' + (digit < 10 ? digit : 0);
-		guess /= 10;
-		cbuffer[5] = '\0';
-		tx_start_string(cbuffer);
-		//u_print_string("\n");
-
+		hasUARTMessage = 1;
 		if(guess > answer){
+			writeString(&stringBuffer[0], "High:");
+			writeInt(&stringBuffer[5], guess);
+			writeString(&stringBuffer[10], "\r\n");
 			sendByte('H');
 			state = 2;
 		}
 		else if (guess < answer){
+			writeString(&stringBuffer[0], "Low:");
+			writeInt(&stringBuffer[4], guess);
+			writeString(&stringBuffer[9], "\r\n");
 			sendByte('L');
 			state = 2;
 		}
 		else{
+			writeString(&stringBuffer[0], "Equal:");
+			writeInt(&stringBuffer[6],  guess);
+			writeString(&stringBuffer[11], "\r\n");
 			sendByte('E');
 			state = 0;
 		}
@@ -174,6 +191,13 @@ void interrupt spi_rx_handler(){
 	}
 	case 0:		//finished, wait for button
 		break;
+	}
+
+	if (hasUARTMessage)
+	{
+		//send uart info
+		uartPrint(stringBuffer);
+		hasUARTMessage = 0;
 	}
 
 	IFG2 &= ~UCB0RXIFG;		 // clear UCB0 RX flag
@@ -200,20 +224,12 @@ void main(){
 
 	WDTCTL = WDTPW + WDTHOLD;       // Stop watchdog timer
 	BCSCTL1 = CALBC1_8MHZ;			// 8Mhz calibration for clock
-  	DCOCTL  = CALDCO_8MHZ;
-  	init_spi();
-  	init_button();
-  	init_WDT();
-  	init_USCI_UART();
-  	srand(genRandom());
+	DCOCTL  = CALDCO_8MHZ;
+	init_spi();
+	init_button();
+	init_WDT();
+	uartInit();
+	srand(genRandom());
 
-  	// Setup printing
-  	conPrintCounter=conPrint;
-  	conPrintSequence=1;
-  	_bis_SR_register(GIE); //enable interrupts so we can print.
-
-  	u_print_string("Game:");
-  	u_print_string(cbuffer);
-
- 	_bis_SR_register(LPM0_bits);
+	_bis_SR_register(LPM0_bits + GIE);
 }
